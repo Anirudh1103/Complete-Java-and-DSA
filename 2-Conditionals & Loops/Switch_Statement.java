ad02898b252972680eea9bc4739bb61a5e04b083
Anirudh C M <161741952+Anirudh1103@users.noreply.github.com>
2024-04-20 13:33:05 +0530

    Update Switch_Statement.java

--- 2-Conditionals & Loops/Switch_Statement.java
+++ 2-Conditionals & Loops/Switch_Statement.java
@@ -2,6 +2,7 @@ public class Switch_Statement {
     public static void main(String[] args) {
         //Old syntax
         String fruit = "Watermelon";
+        System.out.println("Old syntax of swicth statemnet in Java: ");
         switch (fruit)
         {
             case "Apple":
@@ -15,6 +16,16 @@ public class Switch_Statement {
                 break;
             default:
                 System.out.println("Entered string is not a fruit");
+        }
+        //New syntax of switch  in java or Enhanced syntax
+        System.out.println("Enhanced switch statement or New Syntax of Switch statement in java: ");
+        switch (fruit)
+        {
+            case "Mango" -> System.out.println("A yellow juicy & tasty fruit");
+            case "Apple" ->System.out.println("A red crunchy fruit.");
+            case "Watermelon" -> System.out.println("Hard from the outside soft and juicy from the inside!");
+            default -> System.out.println("Entered string is not a fruit");
+
         }
     }
 
